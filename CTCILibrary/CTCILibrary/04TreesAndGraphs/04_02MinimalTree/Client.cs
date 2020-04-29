fc451768e2ea88b7924847b1e3c3677423b2556c
Subodh Shakya <subodhshakya@live.com>
2020-04-29 15:08:40 -0500

    Chapter 04, Problem 02

---
+++ CTCILibrary/CTCILibrary/04TreesAndGraphs/04_02MinimalTree/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary._04TreesAndGraphs._04_02MinimalTree
+{
+    public class Client
+    {
+        public void Run()
+        {
+            int[] sortedArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            MinimalTree minimalTree = new MinimalTree();
+            TreeNode rootNode = minimalTree.CreateMinimalBST(sortedArray);
+        }
+    }
+}
---
+++ CTCILibrary/CTCILibrary/04TreesAndGraphs/04_02MinimalTree/MinimalTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary._04TreesAndGraphs._04_02MinimalTree
+{
+    public class MinimalTree
+    {
+        public TreeNode CreateMinimalBST(int[] sortedArray)
+        {
+            return CreateMinimalBST(sortedArray, 0, sortedArray.Length - 1);
+        }
+
+        public TreeNode CreateMinimalBST(int[] sortedArray, int start, int end)
+        {
+            if (end < start)
+            {
+                return null;
+            }
+
+            int mid = (start + end) / 2;
+            TreeNode n = new TreeNode(sortedArray[mid]);
+            n.Left = CreateMinimalBST(sortedArray, start, mid - 1);
+            n.Right = CreateMinimalBST(sortedArray, mid + 1, end);
+            return n;
+        }
+    }
+}
