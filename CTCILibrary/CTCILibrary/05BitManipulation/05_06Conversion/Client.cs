cda50d8124288f5a64961823fc699724eb141e98
Subodh Shakya <subodhshakya@live.com>
2020-05-31 18:51:27 -0500

    Ch05_06

---
+++ CTCILibrary/CTCILibrary/05BitManipulation/05_06Conversion/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary._05BitManipulation._05_06Conversion
+{
+    public static class Client
+    {
+        public static void Run()
+        {
+            Console.WriteLine(Conversion.BitSwapRequired1(29, 15));
+            Console.WriteLine(Conversion.BitSwapRequired2(29, 15));
+        }
+    }
+}
---
+++ CTCILibrary/CTCILibrary/05BitManipulation/05_06Conversion/Conversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary._05BitManipulation._05_06Conversion
+{
+    public static class Conversion
+    {
+        public static int BitSwapRequired1(int a, int b)
+        {
+            int count = 0;
+            for (int c = a ^ b; c != 0; c = c >> 1)
+            {
+                count += c & 1;
+            }
+
+            return count;
+        }
+
+        public static int BitSwapRequired2(int a, int b)
+        {
+            int count = 0;
+            for (int c = a ^ b; c != 0; c = c & (c - 1)) //c & (c - 1) will clears the least significant bit in c.
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
