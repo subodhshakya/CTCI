a9e045599ec39c78bcf0dbd6e2532b12742b2cbd
Subodh Shakya <subodhshakya@live.com>
2020-05-31 18:27:40 -0500

    Ch05_05

---
+++ CTCILibrary/CTCILibrary/05BitManipulation/05_05Debugger/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary._05BitManipulation._05_05Debugger
+{
+    class Question
+    {
+        /* Debugger: Explain what the following code does: (n & (n-1)) == 0
+         * 
+         * This is not programming question. Explanation of answer is in the book.
+         * Answer: (n & (n-1)) == 0 checks if n is a power of 2 (or if n is 0)
+         * 
+         */
+    }
+}
