2024324c3f097ea00db52ee46c59271003460eb3
Subodh Shakya <subodhshakya@live.com>
2020-09-24 18:03:47 -0500

    Length of Last Word.

---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/058_Length_Of_Last_Word/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._058_Length_Of_Last_Word
+{
+    class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            int previousLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i != 0 && s[i - 1] == ' ' && s[i] != ' ')
+                {
+                    previousLength = 0;
+                }
+                if (s[i] != ' ')
+                    previousLength++;
+            }
+            return previousLength;
+        }
+    }
+}
