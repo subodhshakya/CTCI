9c7051b96e07d9a3c03d59e965e9f60abbdb1fa8
Subodh Shakya <subodhshakya@live.com>
2020-09-26 22:55:27 -0500

    Path sum solved!

--- CTCILibrary/CTCIConsole/Program.cs
+++ CTCILibrary/CTCIConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CTCILibrary;
+using CTCILibrary.YouTubeDemos.LeetCode.Common;
 
 namespace CTCIConsole
 {
@@ -11,7 +12,17 @@ namespace CTCIConsole
     {
         static void Main(string[] args)
         {
-            //
+            TreeNode root = new TreeNode(5);
+            root.left = new TreeNode(1);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(7);
+            root.left.right = new TreeNode(15);
+
+            root.right.left = new TreeNode(9);
+            root.right.right = new TreeNode(11);
+            root.right.right.right = new TreeNode(17);
+
+            CTCILibrary.YouTubeDemos.LeetCode.Easy._112_Path_Sum.Solution.HasPathSum(root, 17);
         }
     }
 }
---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/112_Path_Sum/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CTCILibrary.YouTubeDemos.LeetCode.Common;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._112_Path_Sum
+{
+
+    /* Test data
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(7);
+        root.left.right = new TreeNode(15);
+
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(11);
+        root.right.right.right = new TreeNode(17);     
+     */
+    public static class Solution
+    {
+        public static bool hasPathSum = false;
+        public static bool HasPathSum(TreeNode root, int sum)
+        {
+            PathSumHelper(root, sum, 0);
+            return hasPathSum;
+        }
+
+        public static void PathSumHelper(TreeNode n, int targetSum, int currentSum)
+        {
+            if (n == null || hasPathSum)
+            {
+                return;
+            }
+            currentSum += n.val;
+            if (n.left == null && n.right == null)
+            {
+                if (currentSum == targetSum)
+                {
+                    hasPathSum = true;
+                }
+                return;
+            }            
+            
+            PathSumHelper(n.left, targetSum, currentSum);
+            PathSumHelper(n.right, targetSum, currentSum);            
+        }
+    }
+}
