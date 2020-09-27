7370da550db0d50d147ea1741fac970f020b0b3a
Subodh Shakya <subodhshakya@live.com>
2020-09-27 13:11:31 -0500

    Pascal triangle solved.

--- CTCILibrary/CTCIConsole/Program.cs
+++ CTCILibrary/CTCIConsole/Program.cs
@@ -12,17 +12,10 @@ namespace CTCIConsole
     {
         static void Main(string[] args)
         {
-            TreeNode root = new TreeNode(5);
-            root.left = new TreeNode(1);
-            root.right = new TreeNode(3);
-            root.left.left = new TreeNode(7);
-            root.left.right = new TreeNode(15);
-
-            root.right.left = new TreeNode(9);
-            root.right.right = new TreeNode(11);
-            root.right.right.right = new TreeNode(17);
-
-            CTCILibrary.YouTubeDemos.LeetCode.Easy._112_Path_Sum.Solution.HasPathSum(root, 17);
+            CTCILibrary.YouTubeDemos.LeetCode.Easy._118_Pascals_Triangle.Solution.Generate(1);
+            CTCILibrary.YouTubeDemos.LeetCode.Easy._118_Pascals_Triangle.Solution.Generate(2);
+            CTCILibrary.YouTubeDemos.LeetCode.Easy._118_Pascals_Triangle.Solution.Generate(3);
+            CTCILibrary.YouTubeDemos.LeetCode.Easy._118_Pascals_Triangle.Solution.Generate(5);
         }
     }
 }
---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/118_Pascals_Triangle/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._118_Pascals_Triangle
+{
+    public static class Solution
+    {
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            List<List<int>> pTriangle = new List<List<int>>();
+            if (numRows == 0)
+            {
+                pTriangle.ToArray();
+            }            
+            else
+            {
+                for (int i = 0; i < numRows; i++)
+                {
+                    if (i == 0)
+                    {
+                        pTriangle.Add(new List<int> { 1 });
+                    }
+                    else if (i == 1)
+                    {                        
+                        pTriangle.Add(new List<int> { 1, 1 });
+                    }
+                    else
+                    {
+                        List<int> currentList = new List<int>();
+                        for (int j = 0; j < (i + 1); j++)
+                        {
+                            if (j == 0 || j == i)
+                            {
+                                currentList.Add(1);
+                            }
+                            else
+                            {
+                                currentList.Add(pTriangle[i - 1][j - 1] + pTriangle[i - 1][j]);
+                            }
+                        }
+                        pTriangle.Add(currentList);
+                    }                    
+                }
+            }
+
+            return pTriangle.ToArray();
+        }
+    }
+}
