1dc5e5da77716994171f94d1713fb6050fafb606
Subodh Shakya <subodhshakya@live.com>
2020-09-28 19:29:23 -0500

    Added single number solution and linked list cycle detection solution.

---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/136_Single_Number/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._136_Single_Number
+{
+    public static class Solution
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            HashSet<int> numberSet = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numberSet.Contains(nums[i]))
+                {
+                    numberSet.Remove(nums[i]);
+                }
+                else
+                {
+                    numberSet.Add(nums[i]);
+                }
+            }
+            return numberSet.First();
+        }
+
+        /// <summary>
+        /// This uses XOR operator
+        /// Main logic
+        /// a XOR 0 = a
+        /// a XOR a = 0
+        /// a XOR b XOR b = a
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNumberOptimized(int[] nums)
+        {
+            int a = 0;
+            foreach (int n in nums)
+            {
+                a = a ^ n;
+            }
+            return a;
+        }
+    }
+}
---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/141_Linked_List_Cycle/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CTCILibrary.YouTubeDemos.LeetCode.Common;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._141_Linked_List_Cycle
+{
+    /*
+     *  Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+        There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+        Return true if there is a cycle in the linked list. Otherwise, return false
+     */
+    public static class Solution
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
