73614789ba0293045ce5e63dd10a6c9bd21ef0d1
Subodh Shakya <subodhshakya@live.com>
2020-09-29 19:11:53 -0500

    LeetCode solution 168 and 169

---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/168_Excel_Sheet_Column_Title/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._168_Excel_Sheet_Column_Title
+{
+    public class Solution
+    {
+        public string ConvertToTitle(int n)
+        {
+            String columnName = "";
+
+            while (n > 0)
+            {
+                int rem = n % 26;
+
+                // If remainder is 0, then a 
+                // 'Z' must be there in output 
+                if (rem == 0)
+                {
+                    columnName = "Z" + columnName;
+                    n = (n / 26) - 1;
+                }
+
+                // If remainder is non-zero 
+                else
+                {
+                    columnName = (char)((rem - 1) + 'A') + columnName;
+                    n = n / 26;
+                }
+            }
+
+            return columnName.ToString();
+        }
+
+        /// <summary>
+        /// This is other way around. i.e. Title to number
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>        
+        public int titleToNumber(string s)
+        {
+            // This process is similar to  
+            // binary-to-decimal conversion  
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                result *= 26;
+                result += s[i] - 'A' + 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ CTCILibrary/CTCILibrary/YouTubeDemos/LeetCode/Easy/169_Majority_Number/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTCILibrary.YouTubeDemos.LeetCode.Easy._169_Majority_Number
+{
+    /*
+     * 169. Majority Element
+     * 
+     * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+        You may assume that the array is non-empty and the majority element always exist in the array.
+
+        Example 1:
+
+        Input: [3,2,3]
+        Output: 3
+        Example 2:
+
+        Input: [2,2,1,1,1,2,2]
+        Output: 2
+     * 
+     */
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> numberToCountMap = new Dictionary<int, int>();
+            int majorityNumber = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numberToCountMap.ContainsKey(nums[i]))
+                {
+                    numberToCountMap[nums[i]] += 1;
+                }
+                else
+                {
+                    numberToCountMap.Add(nums[i], 1);
+                }
+                if (numberToCountMap[nums[i]] > (nums.Length / 2))
+                {
+                    majorityNumber = nums[i];
+                    break;
+                }
+            }
+            return majorityNumber;
+        }
+    }
+}
